65a6778f3b7c983ffb8e8c73e25d82df44cc941c
Josip Vojak <josipvojak@gmail.com>
2016-06-24 13:43:53 +0200

    Separated js and php files

--- index.php
+++ index.php
@@ -24,49 +24,7 @@
 
 <script type="text/javascript" src="static/js/jquery.min.js"></script>
 <script type="text/javascript" src="static/js/bootstrap.min.js"></script>
-<script type="text/javascript">
-
-$(document).ready(function() {
-	$("#given_chars").val("");
-});
-
-$(document).keydown(function(e) {
-	console.log(e.keyCode);
-	if(e.keyCode == 8) {
-		$("#given_chars").val($("#given_chars").val().slice(0,-1));
-		return false;
-	}
-
-	if(e.charCode != 32)
-		$("#given_chars").val($("#given_chars").val()+e.key);
-
-	updatePictures();
-
-});
-
-function updatePictures() {
-	var value = $("#given_chars").val();
-	var value_length = value.length;
-
-	$(".hero_pic").each(function() {
-		if(value_length > 0) {
-			hero_name = $(this).attr("data-name");
-			console.log("Value : "+value+" = "+hero_name.substring(0, value_length));
-			if(value == hero_name.substring(0, value_length)) {
-				$(this).removeClass("negative");
-				$(this).addClass("positive");
-			} else {
-				$(this).removeClass("positive");
-				$(this).addClass("negative");
-			}
-		} else {
-			$(this).removeClass("positive");
-			$(this).removeClass("negative");
-		}
-	});
-}
-
-</script>
+<script type="text/javascript" src="static/js/index.js"></script>
 
 </body>
 </html>
